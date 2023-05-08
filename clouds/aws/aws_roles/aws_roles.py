d40c1c8c804ab276620648a03580719ee0bba479
oharan2 <oharan@redhat.com>
2023-05-08 12:44:48 +0300

    Delete apply_update and change logs

--- clouds/aws/aws_roles/aws_roles.py
+++ clouds/aws/aws_roles/aws_roles.py
@@ -1,6 +1,5 @@
 import botocore.errorfactory
 from aws.aws_clients import AWSClient
-import boto3
 from botocore.exceptions import BotoCoreError
 from simple_logger.logger import get_logger
 
@@ -11,7 +10,7 @@ def get_iam_client():
     return AWSClient(service_name="iam").client
 
 
-def create_role_policy(role_name, policy_name, policy_document_path, apply_update=False):
+def create_role_policy(role_name, policy_name, policy_document_path):
     """
     Create the role policy with specific configuration.
 
@@ -19,32 +18,32 @@ def create_role_policy(role_name, policy_name, policy_document_path, apply_updat
         role_name (str): role policy name
         policy_name (str): policy name
         policy_document_path (str): path to Json file that holds the policy documents
-        apply_update (bool): True to update the role policy if found, else False
 
     Raises:
         OSError: If file read failed
         BotoCoreError: If POST request failed
     """
     client = get_iam_client()
-    LOGGER.info(f"Creating new role {role_name} for policy {policy_name}.")
-    if apply_update or not check_if_role_policy_exists(iam_client=client, role_name=role_name):
-        try:
-            with open(policy_document_path, "r") as fd:
-                policy_document = fd.read()
-        except OSError:
-            LOGGER.error(
-                "Json policy document couldn't load. Please validate file path."
-            )
-            raise
-        try:
-            client.put_role_policy(
-                RoleName=role_name,
-                PolicyName=policy_name,
-                PolicyDocument=policy_document,
-            )
-        except BotoCoreError as exc:
-            LOGGER.error(f"Failed with role policy creation:\n{exc}")
-            raise
+    exists = check_if_role_policy_exists(iam_client=client, role_name=role_name)
+    if exists:
+        LOGGER.info(f"Updating role by documented policy in {policy_document_path}.")
+    else:
+        LOGGER.info(f"Creating new role {role_name} for policy {policy_name}.")
+    try:
+        with open(policy_document_path, "r") as fd:
+            policy_document = fd.read()
+    except OSError:
+        LOGGER.error("Json policy document couldn't load. Please validate file path.")
+        raise
+    try:
+        client.put_role_policy(
+            RoleName=role_name,
+            PolicyName=policy_name,
+            PolicyDocument=policy_document,
+        )
+    except BotoCoreError as exc:
+        LOGGER.error(f"Failed with role policy management:\n{exc}")
+        raise
 
 
 def check_if_role_policy_exists(iam_client, role_name):
