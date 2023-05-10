7761d9962c25c79811d9cc3fb18f2240e9eb19c3
oharan2 <oharan@redhat.com>
2023-05-10 13:08:32 +0300

    Specify service_name in client session

--- clouds/aws/aws_clients.py
+++ clouds/aws/aws_clients.py
@@ -5,18 +5,13 @@ from simple_logger.logger import get_logger
 LOGGER = get_logger(name=__name__)
 
 
-def aws_client(region="us-east-1", **kwargs):
+def aws_client(service_name, region="us-east-1", **kwargs):
     """Create an AWS client and manage its resources.
 
-    Notes:
-        When `service_name` is not given, the module uses the default AWS client session with boto3.client(),
-        a low-level service client which doesn't get service_name argument.
-        Therefore - using `service_name=None` returns with an error.
-
     Args:
+        service_name (str): The name of a service, e.g. 's3' or 'ec2'.
         region (str): Region to use for session, a client is associated with a single region. Defaults to us-east-1.
         **kwargs (Any): Supported arguments to initialize a service client.
-            - service_name (str): The name of a service, e.g. 's3' or 'ec2'.
             - api_version (str): The API version to use.  By default, botocore will
                 use the latest API version.
             - use_ssl (bool): Whether or not to use SSL. Defaulted to True.
@@ -31,16 +26,14 @@ def aws_client(region="us-east-1", **kwargs):
             - config (botocore.client.Config): Advanced client configuration options.
 
     Returns:
-        Service client instance.
+        Service client instance. (E.g. botocore.client.IAM)
 
     Raises:
         BotoCoreError: If Failed with creating an AWS client session request.
     """
-    LOGGER.info(
-        f"Creating {kwargs.get('service_name') or 'AWS'} client using region {region}."
-    )
+    LOGGER.info(f"Creating {service_name} client using region {region}.")
     try:
-        return boto3.client(region_name=region, **kwargs)
+        return boto3.client(service_name=service_name, region_name=region, **kwargs)
     except BotoCoreError as exc:
         LOGGER.error(f"Failed to connect with AWS client.\n{exc}")
         raise
