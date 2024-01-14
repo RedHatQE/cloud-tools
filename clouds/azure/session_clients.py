72154f706ffbcca9280cb6fd707f36399c21eb3f
Omri Bar-Haim <obarhaim@redhat.com>
2024-01-14 17:17:38 +0200

    Adding Azure cloud tools support

--- clouds/azure/session_clients.py
+++ clouds/azure/session_clients.py
@@ -1,29 +1,58 @@
-from azure.identity import ClientSecretCredential
-from azure.mgmt.subscription import SubscriptionClient
+import os
+from azure.identity import DefaultAzureCredential
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.redhatopenshift import AzureRedHatOpenShiftClient
+from vars import azure_client_credentials_env_vars
 
 
-def azure_credentials(tenant_id, client_id, client_secret) -> ClientSecretCredential:
-    return ClientSecretCredential(
-        tenant_id=tenant_id,
-        client_id=client_id,
-        client_secret=client_secret,
+class MissingAzureCredentials(Exception):
+    pass
+
+
+def assert_azure_credentials():
+    azure_required_env_vars = azure_client_credentials_env_vars.values()
+    if not all([os.getenv(azure_env_var) for azure_env_var in azure_required_env_vars]):
+        raise MissingAzureCredentials(f"Some required environment variables for Azure clients auth are missing, "
+                                      f"required keys: {azure_required_env_vars}")
+
+
+def get_subscription_id():
+    return os.getenv(azure_client_credentials_env_vars["subscription_id"])
+
+
+def get_azure_credentials():
+    # TODO: check how to get credentials using an identity provider.
+    assert_azure_credentials()
+    return DefaultAzureCredential(
+        exclude_environment_credential=True,
+        exclude_managed_identity_credential=True,
+        exclude_developer_cli_credential=True,
+        exclude_shared_token_cache_credential=True,
+        exclude_powershell_credential=True,
+        exclude_cli_credential=False,
+        exclude_interactive_browser_credential=True,
+        exclude_visual_studio_code_credential=True,
+        exclude_workload_identity_credential=True,
     )
 
 
-def aro_client(credential: ClientSecretCredential, subscription_id: str) -> AzureRedHatOpenShiftClient:
-    return AzureRedHatOpenShiftClient(credential=credential, subscription_id=subscription_id)
+def get_aro_client(credential=None):
+    return AzureRedHatOpenShiftClient(
+        credential=credential,
+        subscription_id=get_subscription_id()
+    )
 
 
-def network_client(credential: ClientSecretCredential, subscription_id: str) -> NetworkManagementClient:
-    return NetworkManagementClient(credential=credential, subscription_id=subscription_id)
+def get_network_client(credential=None):
+    return NetworkManagementClient(
+        credential=credential,
+        subscription_id=get_subscription_id()
+    )
 
 
-def resource_client(credential: ClientSecretCredential, subscription_id: str) -> ResourceManagementClient:
-    return ResourceManagementClient(credential=credential, subscription_id=subscription_id)
-
-
-def subscription_client(credential: ClientSecretCredential) -> SubscriptionClient:
-    return SubscriptionClient(credential=credential)
+def get_resource_client(credential=None):
+    return ResourceManagementClient(
+        credential=credential,
+        subscription_id=get_subscription_id()
+    )
