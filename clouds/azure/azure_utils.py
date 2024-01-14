460c597e37077751364af65cb38580d9ce93681e
Omri Bar-Haim <obarhaim@redhat.com>
2024-01-14 18:03:24 +0200

    Adding Azure cloud tools support

--- clouds/azure/azure_utils.py
+++ clouds/azure/azure_utils.py
@@ -1,12 +1,13 @@
-from session_clients import get_resource_client, get_network_client
+from session_clients import get_resource_client, get_network_client, get_azure_credentials
 from simple_logger.logger import get_logger
 
 LOGGER = get_logger(name=__name__)
 
 
 def cleanup_azure_resources():
-    resource_client = get_resource_client()
-    network_client = get_network_client()
+    credential = get_azure_credentials()
+    resource_client = get_resource_client(credential=credential)
+    network_client = get_network_client(credential=credential)
 
     for resource_group_name in [resource_group.name for resource_group in resource_client.resource_groups.list()]:
         LOGGER.info(f"Deleting resources in {resource_group_name} resource group")
@@ -19,4 +20,5 @@ def cleanup_azure_resources():
             )
         LOGGER.info(f"Deleting {resource_group_name} resource group")
         resource_client.resource_groups.begin_delete(resource_group_name=resource_group_name)
+
     LOGGER.info("All Azure resources deleted successfully.")
---
+++ clouds/azure/main.py
@@ -0,0 +1,10 @@
+from azure_utils import *
+from session_clients import *
+
+
+def main():
+    cleanup_azure_resources()
+
+
+if __name__ == "__main__":
+    main()
