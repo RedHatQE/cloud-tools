d408676ecb9e0fd58aae2b8cced38afec5f7eeb7
Omri Bar-Haim <obarhaim@redhat.com>
2024-01-18 15:16:28 +0200

    Add authorization client, change auth method to env vars

--- clouds/azure/session_clients.py
+++ clouds/azure/session_clients.py
@@ -4,6 +4,7 @@ from azure.mgmt.subscription import SubscriptionClient
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.redhatopenshift import AzureRedHatOpenShiftClient
+from azure.mgmt.authorization import AuthorizationManagementClient
 from clouds.azure.vars import azure_client_credentials_env_vars
 
 
@@ -30,12 +31,12 @@ def get_azure_credentials():
     # TODO: check how to get credentials using an identity provider.
     assert_azure_credentials()
     return DefaultAzureCredential(
-        exclude_environment_credential=True,
+        exclude_environment_credential=False,
         exclude_managed_identity_credential=True,
         exclude_developer_cli_credential=True,
         exclude_shared_token_cache_credential=True,
         exclude_powershell_credential=True,
-        exclude_cli_credential=False,
+        exclude_cli_credential=True,
         exclude_interactive_browser_credential=True,
         exclude_visual_studio_code_credential=True,
         exclude_workload_identity_credential=True,
@@ -52,6 +53,10 @@ def get_network_client(credential=None):
 
 def get_resource_client(credential=None):
     return ResourceManagementClient(credential=credential, subscription_id=get_subscription_id())
+
+
+def get_authorization_client(credential=None):
+    return AuthorizationManagementClient(credential=credential, subscription_id=get_subscription_id())
 
 
 def get_subscription_client(credential=None):
