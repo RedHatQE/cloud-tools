a3e018356c67cf173e6514c516752bf8b94b2515
Omri Bar-Haim <obarhaim@redhat.com>
2024-04-07 11:35:02 +0300

    adding typing to all session client functions

--- clouds/azure/session_clients.py
+++ clouds/azure/session_clients.py
@@ -5,7 +5,7 @@ from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.redhatopenshift import AzureRedHatOpenShiftClient
 
 
-def azure_credentials(tenant_id, client_id, client_secret):
+def azure_credentials(tenant_id, client_id, client_secret) -> ClientSecretCredential:
     return ClientSecretCredential(
         tenant_id=tenant_id,
         client_id=client_id,
@@ -13,17 +13,17 @@ def azure_credentials(tenant_id, client_id, client_secret):
     )
 
 
-def aro_client(credential, subscription_id):
+def aro_client(credential, subscription_id) -> AzureRedHatOpenShiftClient:
     return AzureRedHatOpenShiftClient(credential=credential, subscription_id=subscription_id)
 
 
-def network_client(credential, subscription_id):
+def network_client(credential, subscription_id) -> NetworkManagementClient:
     return NetworkManagementClient(credential=credential, subscription_id=subscription_id)
 
 
-def resource_client(credential, subscription_id):
+def resource_client(credential, subscription_id) -> ResourceManagementClient:
     return ResourceManagementClient(credential=credential, subscription_id=subscription_id)
 
 
-def subscription_client(credential):
+def subscription_client(credential) -> SubscriptionClient:
     return SubscriptionClient(credential=credential)
