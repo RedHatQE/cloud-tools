92fd7ab06b0da037382692fb6416fc9d39461f26
Omri Bar-Haim <obarhaim@redhat.com>
2024-04-11 12:30:28 +0300

    add typing to azure utils functions, add azure_cleanup

--- clouds/microsoft_azure/utils.py
+++ clouds/microsoft_azure/utils.py
@@ -1,23 +1,31 @@
 from typing import List
 from simple_logger.logger import get_logger
 from azure.mgmt.subscription import SubscriptionClient
+from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.redhatopenshift import AzureRedHatOpenShiftClient
 
 
-LOGGER = get_logger(name="microsoft_azure-utils")
+LOGGER = get_logger(name=__name__)
 
 
 def get_aro_supported_versions(aro_client: AzureRedHatOpenShiftClient, region: str) -> List[str]:
-    supported_versions: List[str] = [
-        aro_version.version for aro_version in aro_client.open_shift_versions.list(location=region)
-    ]
+    supported_versions = [aro_version.version for aro_version in aro_client.open_shift_versions.list(location=region)]
     LOGGER.info(f"ARO supported versions: {supported_versions}")
     return supported_versions
 
 
 def get_azure_supported_regions(subscription_client: SubscriptionClient, subscription_id: str) -> List[str]:
-    supported_regions: List[str] = [
+    supported_regions = [
         region.name for region in subscription_client.subscriptions.list_locations(subscription_id=subscription_id)
     ]
     LOGGER.info(f"ARO supported regions: {supported_regions}")
     return supported_regions
+
+
+def azure_cleanup(resource_client: ResourceManagementClient, subscription_id: str) -> None:
+    LOGGER.info("Starting Azure cleanup")
+    for resource_group_name in [resource_group.name for resource_group in resource_client.resource_groups.list()]:
+        LOGGER.info(f"Deleting resource group {resource_group_name}")
+        resource_client.resource_groups.begin_delete(resource_group_name=resource_group_name)
+
+    LOGGER.info("Azure cleanup completed successfully")
