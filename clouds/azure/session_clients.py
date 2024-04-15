e227a5df12bb300f8f245804754104e1ff5e6b36
Omri Bar-Haim <obarhaim@redhat.com>
2024-04-15 12:36:29 +0300

    restoring typing to azure sessions clients

--- clouds/azure/session_clients.py
+++ clouds/azure/session_clients.py
@@ -5,7 +5,7 @@ from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.redhatopenshift import AzureRedHatOpenShiftClient
 
 
-def azure_credentials(tenant_id, client_id, client_secret) -> ClientSecretCredential:
+def azure_credentials(tenant_id: str, client_id: str, client_secret: str) -> ClientSecretCredential:
     return ClientSecretCredential(
         tenant_id=tenant_id,
         client_id=client_id,
@@ -13,17 +13,17 @@ def azure_credentials(tenant_id, client_id, client_secret) -> ClientSecretCreden
     )
 
 
-def aro_client(credential, subscription_id) -> AzureRedHatOpenShiftClient:
+def aro_client(credential: ClientSecretCredential, subscription_id: str) -> AzureRedHatOpenShiftClient:
     return AzureRedHatOpenShiftClient(credential=credential, subscription_id=subscription_id)
 
 
-def network_client(credential, subscription_id) -> NetworkManagementClient:
+def network_client(credential: ClientSecretCredential, subscription_id: str) -> NetworkManagementClient:
     return NetworkManagementClient(credential=credential, subscription_id=subscription_id)
 
 
-def resource_client(credential, subscription_id) -> ResourceManagementClient:
+def resource_client(credential: ClientSecretCredential, subscription_id: str) -> ResourceManagementClient:
     return ResourceManagementClient(credential=credential, subscription_id=subscription_id)
 
 
-def subscription_client(credential) -> SubscriptionClient:
+def subscription_client(credential: ClientSecretCredential) -> SubscriptionClient:
     return SubscriptionClient(credential=credential)
