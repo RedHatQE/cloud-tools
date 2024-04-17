7c9e6a7ec2f87b4fb8c4b849c5c17f26bf5b9afa
Omri Bar-Haim <obarhaim@redhat.com>
2024-04-17 17:14:43 +0300

    initial commit for cloud nuke cli

--- clouds/azure/utils.py
+++ clouds/azure/utils.py
@@ -1,12 +1,10 @@
 from typing import List
 from simple_logger.logger import get_logger
-from azure.core.exceptions import HttpResponseError
 from azure.mgmt.subscription import SubscriptionClient
 from azure.mgmt.redhatopenshift import AzureRedHatOpenShiftClient
-from clouds.azure.session_clients import azure_credentials, resource_client
 
 
-LOGGER = get_logger(name=__name__)
+LOGGER = get_logger(name="azure-utils")
 
 
 def get_aro_supported_versions(aro_client: AzureRedHatOpenShiftClient, region: str) -> List[str]:
@@ -23,45 +21,3 @@ def get_azure_supported_regions(subscription_client: SubscriptionClient, subscri
     ]
     LOGGER.info(f"ARO supported regions: {supported_regions}")
     return supported_regions
-
-
-def nuke_all_azure_resources(tenant_id: str, client_id: str, client_secret: str, subscription_id: str) -> None:
-    """
-    Run nuke for all Azure cloud resources associated with the given credentials.
-
-    Note:
-        This action is irreversible and will permanently delete all resources.
-
-    Args:
-        tenant_id (str): The Azure managed-identity tenant ID.
-        client_id (str): The Azure managed-identity client ID.
-        client_secret (str): The Azure managed-identity client secret.
-        subscription_id (str): The Azure subscription ID.
-    """
-    _resource_client = resource_client(
-        credential=azure_credentials(
-            tenant_id=tenant_id,
-            client_id=client_id,
-            client_secret=client_secret,
-        ),
-        subscription_id=subscription_id,
-    )
-
-    failed_delete_resource_groups: List[str] = []
-    azure_post_cleanup_message = "Azure cleanup completed"
-
-    LOGGER.info("Starting Azure resources cleanup")
-    for resource_group_name in [resource_group.name for resource_group in _resource_client.resource_groups.list()]:
-        try:
-            LOGGER.info(f"Deleting resource group {resource_group_name}")
-            _resource_client.resource_groups.begin_delete(resource_group_name=resource_group_name)
-        except HttpResponseError as ex:
-            LOGGER.error(f"Failed to delete resource group {resource_group_name}: {ex}")
-            failed_delete_resource_groups.append(resource_group_name)
-
-    if failed_delete_resource_groups:
-        LOGGER.info(
-            f"{azure_post_cleanup_message} except for the following resource groups: {failed_delete_resource_groups}"
-        )
-    else:
-        LOGGER.success(azure_post_cleanup_message)
